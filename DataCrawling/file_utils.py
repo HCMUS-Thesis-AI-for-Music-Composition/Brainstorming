3559f89d04b61164e8d2c8b3b1f76cbf4c2c04a7
pqvuong-hcmus <20120406@student.hcmus.edu.vn>
2024-02-03 23:43:47 +0700

    sync

--- DataCrawling/file_utils.py
+++ DataCrawling/file_utils.py
@@ -29,3 +29,12 @@ def write_data_to_json_file(data, file_name):
 def write_data_to_html_file(data, file_name):
     with open(file_name, 'w', encoding='utf-8') as html_file:
         html_file.write(data)
+
+# Append to json file
+def append_data_to_json_file(data, file_name):
+    with open(file_name, 'r+', encoding='utf-8') as json_file:
+        old_data = json.load(json_file)
+        old_data.append(data)
+        json_file.seek(0)
+        json.dump(old_data, json_file, ensure_ascii=False, indent=4)
+        json_file.truncate()
