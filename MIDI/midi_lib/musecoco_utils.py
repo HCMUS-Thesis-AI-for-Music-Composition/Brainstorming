c83eb95923548499eb5f10292572b0b5ef3b9c8c
pqvuong-hcmus <20120406@student.hcmus.edu.vn>
2024-05-29 11:48:32 +0700

    sync

--- MIDI/midi_lib/musecoco_utils.py
+++ MIDI/midi_lib/musecoco_utils.py
@@ -19,35 +19,80 @@
             
 #     return ts_dict
 
+import random
+
 import const.musecoco_const as mcc
 
 def remove_structure_errors(
     musecoco_line: list[tuple],
-    next_acceptable_keys: dict = mcc.next_acceptable_keys
+    next_acceptable_keys: dict = mcc.next_acceptable_keys,
+    should_error_be_fixed_randomly: bool = False
 ) -> list[tuple]:
-    valid_musecoco_line = None
+    valid_musecoco_line = []
     
     prev_key = musecoco_line[0][0]
     
     if prev_key != mcc.str_abbr_time_signature:
         print(f"remove_structure_errors: The first key must be '{mcc.str_abbr_time_signature}'")
+        # valid_musecoco_line.append((mcc.str_abbr_time_signature, 9))
     else:
-        valid_musecoco_line = []
+        pass
+
+    valid_musecoco_line = []
+    
+    valid_musecoco_line.append(musecoco_line[0])
+    
+    for i in range(1, len(musecoco_line)):
+        key, value = musecoco_line[i]
         
-        valid_musecoco_line.append(musecoco_line[0])
-        
-        for i in range(1, len(musecoco_line)):
-            key, value = musecoco_line[i]
+        if key in next_acceptable_keys[prev_key]:
+            if should_error_be_fixed_randomly:
+                v = musecoco_line[i][1]
+
+                if v > 127:
+                    print(f"remove_structure_errors: {key} is not allowed to have a value greater than 127 ({musecoco_line[i]})")
+
+                    musecoco_line[i] = (key, random.randint(10, 25))
+
+                    print(f"-----> remove_structure_errors: ({key}, {random_alternative_value}) is added instead of ({key}, {value})")
+
+                    print()
+                else:
+                    pass
+            else:
+                pass
             
-            if key in next_acceptable_keys[prev_key]:
+            if value > 127:
+                # Ignore the value
+                print(f"remove_structure_errors: {key} is not allowed to have a value greater than 127 ({musecoco_line[i]})")
+            else:
                 valid_musecoco_line.append(musecoco_line[i])
                 prev_key = key
-            else:
-                print(
-                    f"remove_structure_errors: {key} is not allowed after {prev_key} ({musecoco_line[i - 1]} is followed by {musecoco_line[i]})"
+        else:
+            print(f"remove_structure_errors: {key} is not allowed after {prev_key} ({musecoco_line[i - 1]} is followed by {musecoco_line[i]})")
+
+            if should_error_be_fixed_randomly:
+                random_alternative_key = random.choice(next_acceptable_keys[prev_key])
+                random_alternative_value = random.randint(10, 15)
+
+                valid_musecoco_line.append(
+                    (
+                        random_alternative_key, 
+                        random_alternative_value
+                    )
                 )
-                
+
+                print(f"-----> remove_structure_errors: ({random_alternative_key}, {random_alternative_value}) is added instead of ({key}, {value})")
+
+                print()
+
+                prev_key = random_alternative_key
+            else:
+                pass
+
     if len(valid_musecoco_line) == len(musecoco_line):
         print("remove_structure_errors: NO STRUCTURE ERRORS FOUND")
-                
+    else:
+        pass
+
     return valid_musecoco_line
