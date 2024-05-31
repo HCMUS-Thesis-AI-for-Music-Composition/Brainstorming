df55c7b67bba08e51fc67cc9e5026c2fc14f957e
pqvuong-hcmus <20120406@student.hcmus.edu.vn>
2024-05-31 18:59:09 +0700

    sync

--- MIDI/midi_lib/musecoco_utils.py
+++ MIDI/midi_lib/musecoco_utils.py
@@ -27,14 +27,19 @@ def remove_structure_errors(
     musecoco_line: list[tuple],
     next_acceptable_keys: dict = mcc.next_acceptable_keys,
     should_error_be_fixed_randomly: bool = False
-) -> list[tuple]:
+) -> tuple[list[tuple], str]:
+    printed_lines = ""
+
     valid_musecoco_line = []
     
     prev_key = musecoco_line[0][0]
     
     if prev_key != mcc.str_abbr_time_signature:
-        print(f"remove_structure_errors: The first key must be '{mcc.str_abbr_time_signature}'")
+        str_to_print = f"remove_structure_errors: The first key must be '{mcc.str_abbr_time_signature}'"
+        print(str_to_print)
         # valid_musecoco_line.append((mcc.str_abbr_time_signature, 9))
+
+        printed_lines += str_to_print + "\n"
     else:
         pass
 
@@ -50,13 +55,23 @@ def remove_structure_errors(
                 v = musecoco_line[i][1]
 
                 if v > 127:
-                    print(f"remove_structure_errors: {key} is not allowed to have a value greater than 127 ({musecoco_line[i]})")
+                    str_to_print = f"remove_structure_errors: {key} is not allowed to have a value greater than 127 ({musecoco_line[i]})"
+
+                    print(str_to_print)
+
+                    printed_lines += str_to_print + "\n"
 
                     musecoco_line[i] = (key, random.randint(10, 25))
 
-                    print(f"-----> remove_structure_errors: ({key}, {random_alternative_value}) is added instead of ({key}, {value})")
+                    str_to_print = f"-----> remove_structure_errors: ({key}, {random_alternative_value}) is added instead of ({key}, {value})"
+
+                    print(str_to_print)
+
+                    printed_lines += str_to_print + "\n"
 
                     print()
+
+                    printed_lines += "\n"
                 else:
                     pass
             else:
@@ -64,12 +79,19 @@ def remove_structure_errors(
             
             if value > 127:
                 # Ignore the value
-                print(f"remove_structure_errors: {key} is not allowed to have a value greater than 127 ({musecoco_line[i]})")
+                str_to_print = f"remove_structure_errors: {key} is not allowed to have a value greater than 127 ({musecoco_line[i]})"
+                print(str_to_print)
+
+                printed_lines += str_to_print + "\n"
             else:
                 valid_musecoco_line.append(musecoco_line[i])
                 prev_key = key
         else:
-            print(f"remove_structure_errors: {key} is not allowed after {prev_key} ({musecoco_line[i - 1]} is followed by {musecoco_line[i]})")
+            str_to_print = f"remove_structure_errors: {key} is not allowed after {prev_key} ({musecoco_line[i - 1]} is followed by {musecoco_line[i]})"
+
+            print(str_to_print)
+
+            printed_lines += str_to_print + "\n"
 
             if should_error_be_fixed_randomly:
                 random_alternative_key = random.choice(next_acceptable_keys[prev_key])
@@ -82,17 +104,24 @@ def remove_structure_errors(
                     )
                 )
 
-                print(f"-----> remove_structure_errors: ({random_alternative_key}, {random_alternative_value}) is added instead of ({key}, {value})")
+                str_to_print = f"-----> remove_structure_errors: ({random_alternative_key}, {random_alternative_value}) is added instead of ({key}, {value})"
+                print(str_to_print)
+
+                printed_lines += str_to_print + "\n"
 
                 print()
+                printed_lines += "\n"
 
                 prev_key = random_alternative_key
             else:
                 pass
 
     if len(valid_musecoco_line) == len(musecoco_line):
-        print("remove_structure_errors: NO STRUCTURE ERRORS FOUND")
+        str_to_print = "remove_structure_errors: NO STRUCTURE ERRORS FOUND"
+        print(str_to_print)
+
+        printed_lines += str_to_print + "\n"
     else:
         pass
 
-    return valid_musecoco_line
+    return valid_musecoco_line, printed_lines
